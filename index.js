5865a008ca2d69df2e2fa1263889ab7d8daea856
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-07-02 22:36:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = function (stream, callback) {
+  var buffer;
+  var err;
+  stream.on('data', function (data) {
+    buffer = buffer || '';
+    buffer += data;
+  });
+  stream.on('end', function () {
+    callback(err, buffer);
+  });
+  stream.on('error', function (e) {
+    err = e;
+    callback(err, buffer);
+  });
+};
